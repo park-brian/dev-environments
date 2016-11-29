1adbccb713f552610d66193229422bdd9f4e345f
park-brian <re.public@outlook.com>
2016-11-29 13:35:46 -0500

    updated comments

--- ubuntu/nodejs/bootstrap.sh
+++ ubuntu/nodejs/bootstrap.sh
@@ -5,7 +5,7 @@
 ## a new machine
 
 ## To use this script, either run it in the project directory, or specify
-## it as an argument: ./bootstrap.sh /vagrant/projects/demo
+## the absolute path as an argument: ./bootstrap.sh /vagrant/projects/demo
 
 [[ ! -z $1 ]] && DIR="$1/node_modules" || DIR="$PWD/node_modules"
 TMP="/tmp/$(date | md5sum | head -c10)/node_modules"
