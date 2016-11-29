c867291dc561a95377f159134ad87ec6dca53b05
park-brian <re.public@outlook.com>
2016-11-29 13:11:32 -0500

    added workaround for symlinks issue

---
+++ ubuntu/nodejs/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+## This script mounts node_modules as a temporary directory to avoid creating
+## symlinks on windows hosts. You wil need to run this every time you start
+## a new machine
+## To use this script, either run it in the project directory, or specify
+## it as an argument: ./bootstrap.sh /vagrant/projects/demo
+[[ ! -z $1 ]] && DIR="$1/node_modules" || DIR="$PWD/node_modules"
+TMP="/tmp/$(date | md5sum | head -c10)/node_modules"
+sudo mount --bind $TMP $DIR
