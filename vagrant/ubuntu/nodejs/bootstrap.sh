e32e9c3a6bed8407031b11193c39d717c68a8809
park-brian <re.public@outlook.com>
2016-12-08 20:13:49 -0500

    moving Vagrant environments to separate folder

---
+++ vagrant/ubuntu/nodejs/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## This script mounts node_modules as a temporary directory to avoid creating
+## symlinks on windows hosts. You wil need to run this every time you start
+## a new machine
+
+## To use this script, either run it in the project directory, or specify
+## the absolute path as an argument: ./bootstrap.sh /vagrant/projects/demo
+
+[[ ! -z $1 ]] && DIR="$1/node_modules" || DIR="$PWD/node_modules"
+TMP="/tmp/$(date | md5sum | head -c10)/node_modules"
+
+mkdir -p $DIR $TMP
+sudo mount --bind $TMP $DIR
+echo "Mounted $TMP at $DIR"
