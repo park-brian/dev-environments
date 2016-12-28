7e67dfab2ca383eaf0f61d96e2d4071c5c96b8d0
park-brian <re.public@outlook.com>
2016-12-27 22:10:26 -0500

    updating to latest php version

--- docker/centos/drupal/start.bat
+++ docker/centos/drupal/start.bat
@@ -1,5 +1,7 @@
-# example run command
+REM example run command
 
+
+docker stop mysql-drupal drupal-test
 docker rm mysql-drupal drupal-test
 
 docker run --name mysql-drupal -v C:\Development\Web\Data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
